a816bc8529c5a299ceb128079bd5456245ffee9c
FrancescoM95 <mon95fran@gmail.com>
2024-02-24 12:09:57 +0100

    added bootstrap table

--- index.php
+++ index.php
@@ -41,26 +41,55 @@ $hotels = [
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Francesco Mongiello">
+    <meta name="description" content="PHP Hotel">
     <title>PHP Hotel</title>
-</head>
-<body>
-<h1>Elenco degli Hotel</h1>
-    <ul>
-        <?php foreach ($hotels as $hotel) : ?>
-            <li>
-                <h3><?= $hotel['name']?></h3>
 
-                <ul>
-                    <?php foreach ($hotel as $key => $value) : ?>
-                        <li><strong><?= $key ?></strong>: <?= $value ?></li>
-                    <?php endforeach ?>
-                </ul>
-            </li>
-        <?php endforeach ?>
-    </ul>
+     <!--§ font-awesome -->
+     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+        integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=='
+        crossorigin='anonymous'>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container w-75">
+        <h1 class="text-center m-4">Hotels List</h1>
+
+        <table class="table table-hover">
+            <thead>
+                <tr class="fs-4">
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col" class="text-center">Parking</th>
+                <th scope="col" class="text-center">Vote</th>
+                <th scope="col" class="text-center">Distance</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                       <th scope="col"><?= $hotel['name']?></th>
+                       <td><?= $hotel['description'] ?></td>
+                       <td class="text-center">
+                            <?php $icon = $hotel['parking'] ? 'far fa-circle-check fa-lg text-success' : 'far fa-circle-xmark fa-lg text-danger' ?>
+                            <i class="<?= $icon ?>"></i>
+                       </td>
+                       <td class="text-center"><?= $hotel['vote'] ?>/5</td>
+                       <td class="text-center"><?= $hotel['distance'] ?> km</td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </main>
+
 </body>
 </html>
