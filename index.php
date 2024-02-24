61ff24984278a661971000b347a06fff8c24ed73
FrancescoM95 <mon95fran@gmail.com>
2024-02-24 11:28:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance' => 50
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+<h1>Elenco degli Hotel</h1>
+    <ul>
+        <?php foreach ($hotels as $hotel) : ?>
+            <li>
+                <h3><?= $hotel['name']?></h3>
+
+                <ul>
+                    <?php foreach ($hotel as $key => $value) : ?>
+                        <li><strong><?= $key ?></strong>: <?= $value ?></li>
+                    <?php endforeach ?>
+                </ul>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</body>
+</html>
